ac7ea61069fe4e811597c1d3963d63aa2b1cec46
banana-peel <saurabhpitre93@gmail.com>
2017-12-12 03:14:20 -0500

    Test version of Final version of the code. Runs for only 3 iteration before changin epochs

--- cifar_10.py
+++ cifar_10.py
@@ -6,164 +6,206 @@ Created on Mon Dec  4 18:29:30 2017
 @author: saurabh
 """
 
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.autograd import Variable
-from torch.utils.data import DataLoader, Dataset
-from torch.utils.data import sampler
+#import torch
+#import torch.nn as nn
+#import torch.optim as optim
+#from torch.autograd import Variable
+#from torch.utils.data import DataLoader, Dataset
+#from torch.utils.data import sampler
+#
+#import torchvision.datasets as dset
+#import torchvision.transforms as T
+#
+#import numpy as np
+#
+#import timeit
+#
+#import matplotlib.pyplot as plt
+#import load_cifar_data as ld
+#from torchvision.models import resnet
+#import cifar_resnet as cr
 
-import torchvision.datasets as dset
-import torchvision.transforms as T
-
-import numpy as np
-
-import timeit
-
-import matplotlib.pyplot as plt
-import load_cifar_data as ld
-from torchvision.models import resnet
-import cifar_resnet as cr
+import torch.multiprocessing as mp
 
 #Load images
 
-X_train, y_train, X_val, y_val, X_test, y_test = ld.get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=1000,
-                     subtract_mean=True)
+
 
     
-for i in range(4):
-    ax = plt.subplot(1, 4, i + 1)
-    plt.tight_layout()
-    ax.axis('off')
-    plt.imshow(X_train[i])
-    
-plt.show()
 
-
-#Create Dataset
-
-class cifar_data(Dataset):
-    '''
-    Creates the dataset for the cifar data
-    initiates the __len__ and __getitem__
-    '''
-    def __init__(self,images,labels, transform = None):
-        self.images = images
-        self.labels = labels
-        self.transform = transform
-    
-    def __len__(self):
-        return len(self.labels)
-    
-    def __getitem__(self,indx):
-        imgs = self.images[indx]
-        labls = self.labels[indx]
-        
-        if self.transform:
-            imgs, labls = self.transform((imgs,labls))
-        return (imgs,labls)
-    
-req_dataset = cifar_data(X_train,y_train)
-
-# Create the transforms
-
-class ToTensor(object):
-    '''Convert ndarrays in sample to Tensors
-        We don't need to make a tensor out of labels
-        as it is only 1 dim array
-        '''
-
-    def __call__(self, inpt):
-
-        # swap color axis because
-        # numpy image: H x W x C
-        # torch image: C X H X W
-        image,label = inpt
-        image = image.transpose((2, 0, 1))
-        return (torch.from_numpy(image).float(),label)
-
-trfm = ToTensor()
-
-tfm_img, tfm_lab = trfm(req_dataset[10])
-
-# Test the Dataset
-
-transformed_dataset = cifar_data(X_train,y_train,transform=ToTensor())
-validation_data = cifar_data(X_val,y_val,transform=ToTensor())
-test_data = cifar_data(X_test,y_test,transform=ToTensor())
-
-#for i in range(3,7):
-#    sample = transformed_dataset[i]
-#    print(i, sample[0].size(), sample[1],'\n')
-    
-# Create the data loader and test it
-    
-dataloader = DataLoader(transformed_dataset, batch_size = 10, shuffle = True, num_workers=1)
-val_dataloader = DataLoader(validation_data, batch_size = 100, shuffle = True, num_workers=0)
-test_dataloader = DataLoader(test_data, batch_size = 100, shuffle = True, num_workers=0)
-#g= 0
-#for samples in dataloader:
-#    img,labels = samples
-#    print(labels)
-#    g += 1
-#    if g == 3:
-#        break
-    
-#Load the model
-#model = resnet.ResNet(BasicBlock, [2, 2, 2, 2], num_classes = 10)
-model = cr.ResNet56()
-    
-#for i,data in enumerate(dataloader):
-#    inp = Variable(data[0])
-#    y = model(inp)
-#    print(y)
-#    break
-
-# Defining Loss function and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0001)
+#
+## Defining Loss function and optimizer
+#criterion = nn.CrossEntropyLoss()
+#optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0001)
 
 # Training the network
 
-val_img, val_label = next(iter(val_dataloader))
-correct = 0
-total = 0
-for epoch in range(2):  # loop over the dataset multiple times
 
-    running_loss = 0.0
-    for i, data in enumerate(dataloader, 0):
-        # get the inputs
-        inputs, labels = data
 
-        # wrap them in Variable
-        inputs, labels = Variable(inputs), Variable(labels)
+def training_cifar_multi(train_state_dict, val_acc_dict, net_acc_dict ,name,return_top_arg):
+    import torch
+    import torch.nn as nn
+    import torch.optim as optim
+    from torch.autograd import Variable
+#    from torch.utils.data import DataLoader, Dataset
+#    from torch.utils.data import sampler
+    
+#    import torchvision.datasets as dset
+#    import torchvision.transforms as T
+    
+    import numpy as np
+    
+#    import timeit
+    
+#    import matplotlib.pyplot as plt
+    import load_cifar_data as ld
+    import cifar_resnet as cr
+    
+#    import torch.multiprocessing as mp
+    
+    model = cr.ResNet56()
+    
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0001)
+    
+    dataloader, val_dataloader, test_dataloader = ld.create_dataloader()
+    epoch = 0
+    
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.data[0]
-        if i % 1000 == 999:
+    while epoch <= 13:
+        total = 0
+        correct = 0
+        running_loss = 0.0
+        for i, data in enumerate(dataloader, 0):
+            # get the inputs
+            inputs, labels = data
+    
+            # wrap them in Variable
+            inputs, labels = Variable(inputs), Variable(labels)
+    
+            # zero the parameter gradients
+            optimizer.zero_grad()
+    
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+    
+            # print statistics
+            running_loss += loss.data[0]
+            if i % 100 == 99:    # print every 2000 mini-batches
+                print('process =', name, '[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 100))
+                running_loss = 0.0
+            
+            if i==13:
+                break
+    
+    #Saving model to manager
+        train_state_dict[name] = {'state_dict': model.state_dict(), 'optimizer': 
+                        optimizer.state_dict(), 'epoch':epoch}
+        
+            
+        for ix, (val_img,val_label) in enumerate(val_dataloader):
             val_outputs = model(Variable(val_img))
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum()
-            print("Validation accuracy = ", 100*correct/total)
-            total=0
-            correct=0
-        if i % 100 == 99:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 100))
-            running_loss = 0.0
+            _, predicted = torch.max(val_outputs.data, 1)
+            total += val_label.size(0)
+            correct += (predicted == val_label).sum()
+        valid_accuracy = 100*correct/total
+        print("Validation accuracy = ", valid_accuracy)
+        val_acc_dict[name] = valid_accuracy
+        net_acc_dict[name].append(valid_accuracy)
+        total=0
+        correct=0
+    
+        flag = return_top_arg(val_acc_dict, valid_accuracy)
+        if flag:
+            model.load_state_dict(train_state_dict[flag]['state_dict'])
+            optimizer.load_state_dict(train_state_dict[flag]['optimizer'])
+            epoch = train_state_dict[flag]['epoch']
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = (np.random.uniform(0.5,2,1)[0])*param_group['lr']
+        
+        epoch += 1
+            
+        
+        
+        
+        
+        
+        
+        
+        
+def return_top_arg(inp_dict,val_acc):
+    '''
+    Input: a dictionary with the process name and corresponding validatin accuracy
+    Output: a process name randomly taken from the top 20% of the processes if
+            the calling process itself is not in top 20. Else return None
+    '''
+    import operator
+    req_range = sorted(inp_dict.values())[int(len(inp_dict)*0.8):]
+    if val_acc >= req_range[0]:
+        return None
+    sorted_dict = sorted(inp_dict.items(), key=operator.itemgetter(1))
+    indx = np.random.randint(0,len(req_range))
+    req_key = sorted_dict[-indx-1][0]
+    return req_key
+    
+#correct = 0
+#total = 0
+#for epoch in range(2):  # loop over the dataset multiple times
+#
+#    running_loss = 0.0
+#    for i, data in enumerate(dataloader, 0):
+#        # get the inputs
+#        inputs, labels = data
+#
+#        # wrap them in Variable
+#        inputs, labels = Variable(inputs), Variable(labels)
+#
+#        # zero the parameter gradients
+#        optimizer.zero_grad()
+#
+#        # forward + backward + optimize
+#        outputs = model(inputs)
+#        loss = criterion(outputs, labels)
+#        loss.backward()
+#        optimizer.step()
+#
+#        # print statistics
+#        running_loss += loss.data[0]
+#        if i % 1000 == 999:
+#            val_outputs = model(Variable(val_img))
+#            _, predicted = torch.max(outputs.data, 1)
+#            total += labels.size(0)
+#            correct += (predicted == labels).sum()
+#            print("Validation accuracy = ", 100*correct/total)
+#            total=0
+#            correct=0
+#        if i % 100 == 99:    # print every 2000 mini-batches
+#            break
+#            print('[%d, %5d] loss: %.3f' %
+#                  (epoch + 1, i + 1, running_loss / 100))
+#            running_loss = 0.0
+#    break
+#
+#print(optimizer.state_dict())
+#
+#print('Finished Training')
 
-print('Finished Training')
-
-
+if __name__ == "__main__":
+    train_state_dict = mp.Manager().dict()
+    val_acc_dict = mp.Manager().dict()
+    net_acc_dict = mp.Manager().dict()
+    
+    processes = []
+    for rank in range(2):
+        p = mp.Process(target=training_cifar_multi, \
+            args = (train_state_dict, val_acc_dict, net_acc_dict ,rank,return_top_arg))
+        p.start()
+        processes.append(p)
+    for p in processes:
+        p.join()
 
 
